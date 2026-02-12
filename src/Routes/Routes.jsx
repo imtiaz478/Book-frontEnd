e31888b429df3c0eaa9dcb1cea673a2772effddd
binalamimtiaz@gmail.com <binalamimtiaz@gmail.com>
2026-02-12 14:07:02 +0600

    last final

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,6 +6,7 @@ import ErrorPage from '../pages/ErrorPage/ErrorPage';
 import Home from '../pages/Home/Home';
 import About from '../pages/About/About';
 import BookDetails from '../pages/BookDetails/BookDetails';
+import ReadList from '../pages/ReadList/ReadList';
 
 
 
@@ -27,11 +28,17 @@ export const router = createBrowserRouter([
           Component: About
         },
         {
+          path:'/readList',
+          loader:()=>fetch('./booksData.json'),
+          Component:ReadList
+        },
+        {
           path: '/bookDetails/:id',
           loader:()=>fetch('./booksData.json'),
           Component: BookDetails
 
-        }
+        },
+        
       ]
     },
   ]);
